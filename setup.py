c308726cbb4a72a1f33df001b75bd60d6354fae1
danielafriyie <afriyiedaniel1@outlook.com>
2021-09-10 19:54:02 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
