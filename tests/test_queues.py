6d0c1f998f596f15030d534dbf70bb411b24929b
danielafriyie <afriyiedaniel1@outlook.com>
2021-10-06 17:41:55 +0000

    refactored queue tests

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -6,8 +6,8 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-from raccy import ItemUrlScheduler, DatabaseScheduler
-from raccy.core.exceptions import SchedulerError
+from raccy import DatabaseQueue, UrlDownloaderWorker
+from raccy.core.exceptions import QueueError
 
 
 class TestSingleInstance(unittest.TestCase):
