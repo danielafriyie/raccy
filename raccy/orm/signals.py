5da74a7812734bc0feed29b371edf72b58e72dd1
danielafriyie <afriyiedaniel1@outlook.com>
2021-10-18 22:21:41 +0000

    refactored orm.signals module

--- raccy/orm/signals.py
+++ raccy/orm/signals.py
@@ -16,11 +16,11 @@ limitations under the License.
 from raccy.core.signals import Signal
 
 
-class ModelSignal(Signal):
+class ModelPostSaveSignal(Signal):
 
     def notify(self, row_id, model):
         instance = model.objects.get(pk=row_id)
         self._dispatch(instance)
 
 
-post_save = ModelSignal()
+post_save = ModelPostSaveSignal()
