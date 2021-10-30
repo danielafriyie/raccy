ea7df79fb94cc10e50812eb50e210d65e7d21aad
danielafriyie <afriyiedaniel1@outlook.com>
2021-10-30 18:15:02 +0000

    refactored utils.driver module

--- raccy/utils/driver.py
+++ raccy/utils/driver.py
@@ -19,11 +19,9 @@ from logging import Logger
 
 from selenium.common.exceptions import (
     ElementClickInterceptedException, NoSuchElementException, NoSuchAttributeException, WebDriverException,
-    TimeoutException
 )
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 
