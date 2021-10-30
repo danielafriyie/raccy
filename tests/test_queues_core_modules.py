80005be6074b9a9bfdb5205ecacee12f41e3ed4e
danielafriyie <afriyiedaniel1@outlook.com>
2021-10-30 09:54:58 +0000

    added test for signals

--- tests/test_queues_core_modules.py
+++ tests/test_queues_core_modules.py
@@ -107,7 +107,18 @@ class TestSignals(BaseTestClass):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        class Foo:
+            pass
+
+        cls.foo = Foo()
+        cls.signal = Signal()
+
+    def test_unregistered_signal_raises_signal_exception(self):
+        with self.assertRaises(SignalException):
+            self.signal.notify(self.foo)
+
+        with self.assertRaises(SignalException):
+            self.signal.remove_dispatch(self.foo, 'foo')
 
 
 if __name__ == '__main__':
